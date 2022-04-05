26ae0d1aa1f2403585e0e70214e4e142fca043f6
John Oseni <johnoseni26@gmail.com>
2022-04-05 15:23:12 +0100

    fixed it all up sir

--- vue.config.js
+++ vue.config.js
@@ -62,12 +62,12 @@ module.exports = {
     popup: {
       template: 'public/index.html',
       entry: './src/main.js',
-      title: 'Liquality Wallet'
+      title: 'Ecla Wallet'
     },
     standalone: {
       template: 'public/index.html',
       entry: './src/main.js',
-      title: 'Liquality Wallet',
+      title: 'Ecla Wallet',
       filename: 'index.html'
     }
   }
